7b76b362285335616f753780ef838e8c1b5be341
weddify <yusupmabd@gmail.com>
2025-08-20 00:00:06 +0700

    edit landing page and add auth

--- src/routes/auth/confirm/+page.server.ts
+++ src/routes/auth/confirm/+page.server.ts
@@ -4,9 +4,12 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ url }) => {
 	const token_hash = url.searchParams.get('token_hash');
 	const type = url.searchParams.get('type');
+
 	if (token_hash && type) {
-		// Supabase akan otomatis menangani token
+		// Supabase otomatis menangani token
+		// kita cuma redirect ke halaman yang diinginkan
 		throw redirect(303, '/dashboard');
 	}
+
 	throw redirect(303, '/login');
 };
